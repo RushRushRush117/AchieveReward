d00b1c97973fbf35183bc8222b1fc090ff9b4ed9
jiangyang <jiangyang@youkia.net>
2019-07-03 12:46:02 +0900

    完善游戏事件类

--- AchieveReward/AchieveReward/GameEvent.cs
+++ AchieveReward/AchieveReward/GameEvent.cs
@@ -6,10 +6,42 @@ using System.Threading.Tasks;
 
 namespace AchieveReward
 {
+    public class GameEventInfo
+    {
+        public Enum type;
+        public List<GameEventHandler> mHandler;
+        public object[] mArgs;
+        public GameEventInfo(Enum type, GameEventHandler handler, params object[] args)
+        {
+            this.type = type;
+            if (mHandler == null) { mHandler = new List<GameEventHandler>(); }
+            mHandler.Add(handler);
+            mArgs = args;
+        }
+
+        public GameEventInfo(Enum type, List<GameEventHandler> handlers, params object[] argss)
+        {
+            this.type = type;
+            mHandler = handlers;
+            mArgs = argss;
+        }
+        public void ExcuteHandler()
+        {
+            if (mHandler != null)
+            {
+                for (int i = 0; i < mHandler.Count; i++)
+                {
+                    mHandler[i](mArgs);
+                }
+            }
+        }
+    }
     class GameEvent : IGameEvent
     {
         protected Dictionary<Enum, List<GameEventHandler>> mInternalEvent;
         protected Dictionary<Enum, List<GameEventHandler>> mInternalUseOnceEvent;
+        protected List<GameEventInfo> mAsyncHandlerInfos;
+
         public bool IsDispose
         {
             get
@@ -20,6 +52,8 @@ namespace AchieveReward
         public GameEvent()
         {
             mInternalEvent = new Dictionary<Enum, List<GameEventHandler>>();
+            mInternalUseOnceEvent = new Dictionary<Enum, List<GameEventHandler>>();
+            mAsyncHandlerInfos = new List<GameEventInfo>();
         }
 
         public void AddEvent(Enum type, GameEventHandler handler, bool isUseOnce = false, bool isFirst = false)
@@ -43,30 +77,40 @@ namespace AchieveReward
 
         public void DispatchAsyncEvent(Enum type, params object[] args)
         {
-            throw new NotImplementedException();
+            List<GameEventHandler> handlers = null;
+            if (mInternalEvent.TryGetValue(type, out handlers))
+            {
+                mAsyncHandlerInfos.Add(new GameEventInfo(type, handlers, args));
+            }
         }
 
         public void DispatchEvent(Enum type, params object[] args)
         {
             if (mInternalEvent.ContainsKey(type))
             {
+                GameEventHandler handler = null;
                 for (int i = 0; i < mInternalEvent[type].Count; i++)
                 {
-                    GameEventHandler handler = mInternalEvent[type][i];
-                    if(mInternalUseOnceEvent.ContainsKey(type) && mInternalUseOnceEvent[type].Contains(handler)) { }
+                    handler = mInternalEvent[type][i];
                     handler(args);
+                }
+                if (mInternalUseOnceEvent.ContainsKey(type) && mInternalUseOnceEvent[type].Contains(handler))
+                {
+                    RemoveEvent(type, handler);
                 }
             }
         }
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            mInternalUseOnceEvent.Clear();
+            mInternalEvent.Clear();
+            mAsyncHandlerInfos.Clear();
         }
 
         public bool HasEvent(Enum type)
         {
-            throw new NotImplementedException();
+            return mInternalEvent.ContainsKey(type) || mInternalUseOnceEvent.ContainsKey(type);
         }
 
         public void RemoveEvent()
@@ -97,12 +141,35 @@ namespace AchieveReward
                     handlers.Remove(handler);
                 }
             }
-            if(mInternalUseOnceEvent.TryGetValue(type,out handlers))
+            if (mInternalUseOnceEvent.TryGetValue(type, out handlers))
             {
                 if (handlers.Contains(handler))
                 {
                     handlers.Remove(handler);
                 }
+            }
+        }
+
+        public void UpdateEvents()
+        {
+            if (mAsyncHandlerInfos.Count > 0)
+            {
+                for (int i = 0; i < mAsyncHandlerInfos.Count; i++)
+                {
+                    GameEventInfo info = mAsyncHandlerInfos[i];
+                    for (int j = 0; j < info.mHandler.Count; j++)
+                    {
+                        GameEventHandler handler = info.mHandler[j];
+                        handler(info.mArgs);
+                        if (mInternalUseOnceEvent.ContainsKey(mAsyncHandlerInfos[i].type)
+    && mInternalUseOnceEvent[mAsyncHandlerInfos[i].type].Contains(handler))
+                        {
+                            RemoveEvent(info.type, handler);
+                        }
+                    }
+
+                }
+                mAsyncHandlerInfos.Clear();
             }
         }
     }
