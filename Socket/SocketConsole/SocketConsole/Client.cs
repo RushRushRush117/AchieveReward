4b94914393d557ddfaf46e60e8b23c34ec4d84e8
jiangyang <jiangyang@youkia.net>
2019-09-08 14:53:18 +0800

    提交socket工程

---
+++ Socket/SocketConsole/SocketConsole/Client.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketConsole
+{
+    class Client
+    {
+        int port = 6000;
+        string ipAdress = "127.0.0.0";
+        IPAddress ip;
+        IPEndPoint ipe;
+
+        public void NetWorkInit()
+        {
+            ip = IPAddress.Parse(ipAdress);
+            ipe = new IPEndPoint(ip, port);
+            Socket mClientSocket = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            mClientSocket.Connect(ipe);
+            Console.WriteLine("建立连接成功");
+
+            string sendStr = "send to server : hello ";
+            byte[] buffer = Encoding.ASCII.GetBytes(sendStr);
+            mClientSocket.Send(buffer);
+
+            string recStr = "";
+            byte[] _buffer = new byte[4096];
+            mClientSocket.Receive(_buffer,)
+        }
+        
+    }
+}
--- Socket/SocketConsole/SocketConsole/Program.cs
+++ Socket/SocketConsole/SocketConsole/Program.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using System.Text;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
 
 namespace SocketConsole
 {
@@ -10,6 +13,28 @@ namespace SocketConsole
     {
         static void Main(string[] args)
         {
+            int port = 6000;
+            string host = "127.0.0.1";
+            IPAddress ip = IPAddress.Parse(host);
+            IPEndPoint ipe = new IPEndPoint(ip, port);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(ipe);
+            socket.Listen(0);
+            Console.WriteLine("监听已经打开");
+
+            Socket accpetSocket = socket.Accept();
+            Console.WriteLine("连接已经建立");
+            Byte[] buffer = new Byte[4096];
+            accpetSocket.Receive(buffer);
+            string str= Encoding.ASCII.GetString(buffer);
+
+            Console.WriteLine("接受到信息 : " + str);
+
+            string sendStr = "Send to Client :Hello";
+            Byte[] _buffer = Encoding.ASCII.GetBytes(sendStr);
+            accpetSocket.Send(_buffer);
+            accpetSocket.Close();
+            socket.Close();
         }
     }
 }
